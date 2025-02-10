608b1180b8e1400c5907692caa3965f48213496d
Trmd1 <theo.rmd-perso@outlook.com>
2025-02-10 15:11:19 +0100

    Fix recherche via nom du pokemon

--- api/script/main.py
+++ api/script/main.py
@@ -2,13 +2,15 @@ import pymongo
 from typing import Optional
 
 from fastapi import FastAPI
-
+from fastapi.staticfiles import StaticFiles
 from pymongo import MongoClient
 
 app = FastAPI()
 client = pymongo.MongoClient("mongodb://root:example@mongo:27017/")
 db = client.Projet
 collection = db.pokemon
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.get("/")
@@ -27,5 +29,9 @@ def search_dex(dex_num:int):
 
 @app.get("/pokemon/{poke_name}")
 def search_name(poke_name:str):
-    objet=collection.find_one({"name":poke_name},{"_id":0})
+    objet=collection.find_one({"name": {"$regex": f"^{poke_name}$", "$options": "i"}}, {"_id": 0})
+    # print(collection.find_one({"dex":0},{"_id":0})["name"])
+    # boolTest = False
+    # boolTest = (str.lower(poke_name)==str.lower(collection.find_one({"dex":0},{"_id":0})["name"]))
+    # print(boolTest)
     return objet
