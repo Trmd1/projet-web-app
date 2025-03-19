5206016867e079b02820a9c2ee8eca8221660935
Trmd1 <theo.rmd-perso@outlook.com>
2025-03-19 10:44:27 +0100

    add description on doc page

--- api/script/main.py
+++ api/script/main.py
@@ -19,21 +19,17 @@ def read_root():
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
 
-@app.get("/dex/{dex_num}")
+@app.get("/dex/{dex_num}",description="Search a Pokemon by his dex number")
 def search_dex(dex_num:int):
     objet=collection.find_one({"dex":dex_num},{"_id":0})
     return objet
 
-@app.get("/pokemon/{poke_name}")
+@app.get("/pokemon/{poke_name}",description="Search a Pokemon by his name")
 def search_name(poke_name:str):
     objet=collection.find_one({"name": {"$regex": f"^{poke_name}$", "$options": "i"}}, {"_id": 0})
-    # print(collection.find_one({"dex":0},{"_id":0})["name"])
-    # boolTest = False
-    # boolTest = (str.lower(poke_name)==str.lower(collection.find_one({"dex":0},{"_id":0})["name"]))
-    # print(boolTest)
     return objet
 
-@app.get("/types/{poke_type}")
+@app.get("/types/{poke_type}",description="Search which Pokemon share a type")
 def search_type(poke_type:str):
     objets = collection.find(
         {"forms": {"$elemMatch": {"types": str.upper(poke_type)}}},  # Recherche dans la liste
